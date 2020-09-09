0fd08685435a7e056440143cab3ab85915b89e38
Aigars Peda <aigarspeda@gmail.com>
2020-09-09 06:58:06 +0300

    Create UploadForm.tsx

---
+++ src/components/uploadForm/UploadForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UploadForm: React.FC = () => {
+  return (
+    <form>
+      <h1>Ha</h1>
+      <input type="file" />
+    </form>
+  );
+};
+
+export default UploadForm;
