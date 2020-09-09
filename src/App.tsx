19806849cc7c9cc4dcb7b5fde9561d537f457933
Aigars Peda <aigarspeda@gmail.com>
2020-09-09 06:57:45 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./global.styles/App.scss";
 
 // components
 import Title from "./components/Title/Title";
+import UploadForm from "./components/uploadForm/UploadForm";
 
 class App extends React.PureComponent {
   render() {
     return (
       <div className="App">
         <Title />
+        <UploadForm />
       </div>
     );
   }
