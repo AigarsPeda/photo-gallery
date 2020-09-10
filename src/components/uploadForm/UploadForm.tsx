c77be1c96cbaa916e9e1e41c694b3c7c736fb096
Aigars Peda <aigarspeda@gmail.com>
2020-09-10 09:08:16 +0300

    saving added image to sate

--- src/components/uploadForm/UploadForm.tsx
+++ src/components/uploadForm/UploadForm.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm: React.FC = () => {
+  const [file, setFile] = useState<File>();
+
+  const types = ["image/png", "image/jpeg"];
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+    let selected = e.target.files[0];
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+    }
+  };
+
   return (
     <form>
       <h1>Ha</h1>
-      <input type="file" />
+      <input type="file" onChange={changeHandler} />
     </form>
   );
 };
