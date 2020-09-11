5e5c52688671f699ef431e8bfe01f73d6e1fbd21
Aigars Peda <aigarspeda@gmail.com>
2020-09-11 06:47:42 +0300

    updated ImageGrid

--- src/components/imageGrid/ImageGrid.tsx
+++ src/components/imageGrid/ImageGrid.tsx
@@ -4,17 +4,24 @@ import useFirestore from "../../hooks/useFirestore";
 // type
 import { ImageCollection } from "../../types/type";
 
-const ImageGrid: React.FC = () => {
-  const { docs } = useFirestore("images");
+interface Props {
+  setSelectedImg: React.Dispatch<React.SetStateAction<string | undefined>>;
+}
 
-  console.log(docs);
+const ImageGrid: React.FC<Props> = (props) => {
+  const { setSelectedImg } = props;
+  const { docs } = useFirestore("images");
 
   return (
     <div className="image-grid">
       {docs &&
         docs.map((doc: ImageCollection) => {
           return (
-            <div className="img-wrap" key={doc.id}>
+            <div
+              className="img-wrap"
+              key={doc.id}
+              onClick={() => setSelectedImg(doc.url)}
+            >
               <img src={doc.url} alt="uploaded pic" />
             </div>
           );
