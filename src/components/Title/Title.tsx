c9b64c1c5ca409c7a6ea8388586173fb184ee31e
Aigars Peda <aigarspeda@gmail.com>
2020-09-11 06:48:29 +0300

    Update Title.tsx

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -5,7 +5,7 @@ const Title: React.FC = () => {
     <div className="title">
       <h1>Photo Gallery</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Press + and upload a picture.</p>
     </div>
   );
 };
