034879e809477e6b661926362e6a36f054df09e0
Aigars Peda <aigarspeda@gmail.com>
2020-09-14 10:06:33 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./global.styles/App.scss";
 
-// components
-import UploadPage from "./pages/uploadPage";
+// router
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+// pages
+import UploadPage from "./pages/UploadPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <UploadPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={UploadPage} />
+          <Route exact path="/sign-in" component={Login} />
+          <Route exact path="/sign-up" component={Register} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
