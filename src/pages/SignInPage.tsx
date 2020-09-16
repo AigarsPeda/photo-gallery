0caab72d3e4f2a61fa68e36f6f7233aa17e8910b
Aigars Peda <aigarspeda@gmail.com>
2020-09-16 10:00:31 +0300

    Create SignInPage.tsx

---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SignIn from "../components/signIn/SignIn";
+
+const SignInPage: React.FC = () => {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInPage;
