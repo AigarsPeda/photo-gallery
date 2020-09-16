67c021d2e072b0386532c3385c3fa7f386629814
Aigars Peda <aigarspeda@gmail.com>
2020-09-16 06:57:02 +0300

    Create SignUp.tsx

---
+++ src/components/signUp/SignUp.tsx
@@ -0,0 +1,89 @@
+import React, { useCallback, useContext, useState } from "react";
+
+// router
+import {
+  Link,
+  Redirect,
+  RouteComponentProps,
+  withRouter
+} from "react-router-dom";
+
+// firebase
+import { AuthContext } from "../../auth/AuthProvider";
+import { projectFirestore } from "../../firebase/config";
+
+// components
+import Input from "../reusable/input/Input";
+
+interface Props extends RouteComponentProps {}
+
+const SignUp: React.FC<Props> = (props) => {
+  const { history } = props;
+  const { currentUser } = useContext(AuthContext);
+
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: ""
+  });
+  const [error, setError] = useState<string>("");
+
+  const handleSignUp = useCallback(
+    async (event: React.FormEvent<HTMLFormElement>) => {
+      const { email, password } = loginData;
+      event.preventDefault();
+      try {
+        await projectFirestore.app
+          .auth()
+          .createUserWithEmailAndPassword(email, password);
+        history.push("/");
+      } catch (err) {
+        setError("Something went wrong");
+        console.log(err);
+      }
+    },
+    [history, loginData]
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginData((state) => ({
+      ...state,
+      [name]: value
+    }));
+  };
+
+  // if user is already log in
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div className="sign-up">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSignUp}>
+        <Input
+          label="Email"
+          name="email"
+          type="email"
+          placeholder="Email"
+          autoComplete="On"
+          value={loginData.email}
+          handleChange={handleChange}
+        />
+        <Input
+          label="Password"
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={loginData.password}
+          handleChange={handleChange}
+        />
+        {error && <div>{error}</div>}
+        <button type="submit">Log in</button>
+        <Link to="/sign-in">Do You have account?</Link>
+      </form>
+    </div>
+  );
+};
+
+export default withRouter(SignUp);
