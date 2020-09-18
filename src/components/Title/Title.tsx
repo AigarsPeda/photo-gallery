dd1cab09818d6a47069189a1ba3ec15343cafc50
Aigars Peda <aigarspeda@gmail.com>
2020-09-18 06:49:50 +0300

    Updated Title component

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,7 +3,6 @@ import React from "react";
 const Title: React.FC = () => {
   return (
     <div className="title">
-      <h1>Photo Gallery</h1>
       <h2>Your Pictures</h2>
       <p>Press + and upload a picture.</p>
     </div>
